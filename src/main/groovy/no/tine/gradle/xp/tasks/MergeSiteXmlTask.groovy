0a234f45403ea58eea21b4d6544072a9bf820395
Andre Sollie <andre@sollie.info>
2017-11-28 11:36:15 +0100

    - Refactoring.

--- src/main/groovy/no/tine/gradle/xp/tasks/MergeSiteXmlTask.groovy
+++ src/main/groovy/no/tine/gradle/xp/tasks/MergeSiteXmlTask.groovy
@@ -1,5 +1,6 @@
 package no.tine.gradle.xp.tasks
 
+import groovy.util.slurpersupport.GPathResult
 import groovy.xml.XmlUtil
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Internal
@@ -15,31 +16,57 @@ class MergeSiteXmlTask extends DefaultTask {
 
 	@TaskAction
 	def mergeSiteXml() {
+		if (project.configurations.include != null) {
+			this.merge(project.configurations.include)
+		}
+	}
 
-		def original = new XmlSlurper().parse(new File(siteXml))
+	protected void merge(def include) {
+		final  def original = new XmlSlurper().parse(new File(siteXml))
 		def siteFiles = []
 
-		project.configurations.include.each {
-			def configName = it.toString().tokenize('/').last()[0..-5]
-			siteFiles = project.zipTree(it).matching({
-				include 'site/site.xml'
-			}).collect()
+		include.each {
+			siteFiles = this.findSiteXmlFiles(it)
 
 			siteFiles.forEach({
-				def siteLib = new XmlSlurper().parse(it)
-				siteLib.config.children().each{
-					if(it.label) {
-						it.label.replaceBody (it.label.toString() + ' (' + configName + ')')
-					}
-					original.config.appendNode(it)
-				}
-				siteLib['x-data'].each{
-					it.attributes()['lib-src'] = configName
-					original.appendNode(it)
-				}
+				appendToOriginal(it, original)
 			})
 		}
+
 		def writer = new FileWriter(target)
 		XmlUtil.serialize(original, writer)
+	}
+
+	protected void appendToOriginal(it, original) {
+
+		def siteLib = new XmlSlurper().parse(it)
+		def configName = it.toString().tokenize('/').last()[0..-5]
+
+		this.appendToConfig(siteLib, original, configName)
+
+		this.appendXData(siteLib, original, configName);
+
+	}
+
+	protected findSiteXmlFiles(def file) {
+		return project.zipTree(file).matching({
+			include 'site/site.xml'
+		}).collect()
+	}
+
+	protected void appendToConfig(GPathResult siteLib, original, configName) {
+		siteLib.config.children().each{
+			if(it.label) {
+				it.label.replaceBody (it.label.toString() + ' (' + configName + ')')
+			}
+			original.config.appendNode(it)
+		}
+	}
+
+	protected void appendXData(GPathResult siteLib, original, configName) {
+		siteLib['x-data'].each {
+			it.attributes()['lib-src'] = configName
+			original.appendNode(it)
+		}
 	}
 }

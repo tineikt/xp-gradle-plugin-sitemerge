d392b5765d61f85b6e67512dba15e1f33611fbb5
Edward Grönroos <edward@gronroos.se>
2018-05-15 11:14:15 +0200

    Merge filters in site.xml aswell

--- src/test/groovy/no/tine/gradle/xp/SiteMergeModuleTest.groovy
+++ src/test/groovy/no/tine/gradle/xp/SiteMergeModuleTest.groovy
@@ -8,7 +8,7 @@ import groovy.xml.XmlUtil
 class SiteMergeModuleTest extends Specification {
 
 	@Shared
-	def original = '<site><config></config><x-data mixin="tags1" lib-src="site"/><mappings><mapping controller="default3.js" lib-src="site"></mapping></mappings></site>'
+	def original = '<site><config></config><x-data mixin="tags1" lib-src="site"/><mappings><mapping controller="default3.js" lib-src="site"></mapping></mappings><filters><response-filter name="default-response-filter1" order="10" lib-src="site" /></filters></site>'
 
 	@Shared
 	def siteXml =
@@ -24,6 +24,9 @@ class SiteMergeModuleTest extends Specification {
 			'   <mapping controller="default1.js" lib-src="site" merged="xp-gradle-plugin-sitemerge"></mapping>' +
 			'   <mapping controller="default2.js" lib-src="site" merged="xp-gradle-plugin-sitemerge"></mapping>' +
 			' </mappings>' +
+			' <filters>' +
+			'   <response-filter name="default-response-filter1" order="10" lib-src="site" merged="xp-gradle-plugin-sitemerge"></response-filter>' +
+			' </filters>' +
 			'</site>'
 
 	def setup() {
@@ -31,61 +34,72 @@ class SiteMergeModuleTest extends Specification {
 
 	def "AppendXData"() {
 		setup:
-		GPathResult originalFile = new XmlSlurper().parseText(original)
-		GPathResult siteLib = new XmlSlurper().parseText(siteXml)
+			GPathResult originalFile = new XmlSlurper().parseText(original)
+			GPathResult siteLib = new XmlSlurper().parseText(siteXml)
 
 		when:
-		SiteMergeModule.appendXData(siteLib, originalFile, 'xp-lib', 'site')
+			SiteMergeModule.appendXData(siteLib, originalFile, 'xp-lib', 'site')
 
 		then:
-		// println XmlUtil.serialize(originalFile)
-		originalFile[0].children().size() == 4
+			// println XmlUtil.serialize(originalFile)
+			originalFile[0].children().findAll { it.name() == 'x-data' }.size() == 2
 	}
 
 	def "AppendMappings"() {
 		setup:
-		GPathResult originalFile = new XmlSlurper().parseText(original)
-		GPathResult siteLib = new XmlSlurper().parseText(siteXml)
+			GPathResult originalFile = new XmlSlurper().parseText(original)
+			GPathResult siteLib = new XmlSlurper().parseText(siteXml)
 
 		when:
-		SiteMergeModule.appendMappings(siteLib, originalFile, 'xp-lib', 'site')
+			SiteMergeModule.appendMappings(siteLib, originalFile, 'xp-lib', 'site')
 
 		then:
-		// println XmlUtil.serialize(originalFile)
-		originalFile[0].children().findAll { it.name() == 'mappings' }[0].children().size() == 3
+			originalFile[0].children().findAll { it.name() == 'mappings' }[0].children().size() == 3
+	}
+
+	def "AppendFilters"() {
+		setup:
+			GPathResult originalFile = new XmlSlurper().parseText(original)
+			GPathResult siteLib = new XmlSlurper().parseText(siteXml)
+
+		when:
+			SiteMergeModule.AppendFilters(siteLib, originalFile, 'xp-lib', 'site')
+
+		then:
+			originalFile[0].children().findAll { it.name() == 'filters' }[0].children().size() == 1
 	}
 
 	def "Get config name on windows"() {
 		setup:
-		File file = new File("C:\\temp\\site\\site.xml")
+			File file = new File("C:\\temp\\site\\site.xml")
 
 		when:
-		String name = SiteMergeModule.getConfigName(file, "\\")
+			String name = SiteMergeModule.getConfigName(file, "\\")
 
 		then:
-		name == "site"
+			name == "site"
 	}
 
 	def "Get config name on linux"() {
 		setup:
-		File file = new File("/temp/test/site/site.xml")
+			File file = new File("/temp/test/site/site.xml")
 
 		when:
-		String name = SiteMergeModule.getConfigName(file, "/")
+			String name = SiteMergeModule.getConfigName(file, "/")
 
 		then:
-		name == "site"
+			name == "site"
 	}
 
 	def "Get file name on linux"() {
 		setup:
-		File file = new File("/temp/test/site/test.jar")
+			File file = new File("/temp/test/site/test.jar")
 
 		when:
-		String name = SiteMergeModule.getJarName(file)
+			String name = SiteMergeModule.getJarName(file)
 
 		then:
-		name == "test"
+			name == "test"
 	}
 
 
